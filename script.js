4a5845f2b3c6fbd7567924f1f2b56b475229a0f6
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2022-12-19 01:45:43 -0500

    After BCS advice: renaming vars, refactor functions and classes switch

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
-var apiKey = "3071f7a578965b683c18b72b89763dce";
+var placeName = cityInput.getAttribute.value;
+var Print = JSON.parse(localWeather)
+var Condition = JSON.parse.getAttribute(weather.id);
+var butTon = document.querySelector('.weather-button');
+var Params = JSON.parse.getAttribute(weather.main);
+var Descrip = JSON.parse.getAttribute(weather.description);
+var Pic = JSON.parse.getAttribute(weather.icon);
+var Temp = JSON.parse.getAttribute(main.temp);
+var bigBoard = document.querySelector('.city-date-icon');
+var cityInput = document.querySelector('.city-input');
 var weather1 =  document.querySelector('.past1'); 
 var weather2 =  document.querySelector('.past2'); 
 var weather3 =  document.querySelector('.past3'); 
 var weather4 =  document.querySelector('.past4'); 
 var weather5 =  document.querySelector('.past5'); 
 var weather6 =  document.querySelector('.past6'); 
-//JSON.stringify(coord);//{city name},{state code},{country code}//
-var Condition = JSON.parse(weather.id);
-var weatherClick = document.querySelector('.weather-button');
-var Params = JSON.parse(weather.main);
-var Descrip = JSON.parse(weather.description);
-var Pic = JSON.parse(weather.icon);
-var Temp = JSON.parse(main.temp);
-var cityInput = document.querySelector('.city-input');
-var citySend = cityInput.value;
+var apiKey = '3071f7a578965b683c18b72b89763dce';
+
+
 
 var date = document.querySelector('.th-wthr');
 
 
+                        // Event listener on click for search
+butTon.addEventListener('click', (fetchWeather ()));
+
+    function fetchWeather (butTon){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + placeName + '&units=imperial&appid=3071f7a578965b683c18b72b89763dce',{
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow'})
+     
+        .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
 
 
-date.textContent = dayjs().format('MM/DD/YY');
-
-var storedWeather = weather1.textContent;
 
 
-var pastCity = 
-
-async function getWeather() {fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySend + '&units=imperial&appid=' + apiKey)
-.then(response => { 
-console.log(response); }
-}
-
-var weatherPrint = JSON.parse(localWeather)
+date.innerHTML = dayjs().format('MM/DD/YY');
 
 
 
+//var storedWeather = weather1.textContent;
+
+//async function getWeather() {fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySend + '&units=imperial&appid=' + apiKey)
+//.then(response => { 
+//console.log(response); }
+//}
 
 
-weatherClick.addEventListener('click', getWeather)
+
+//function insertName (city) {
+//    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&units=imperial&appid=' + apiKey)
+//    .then(response => { 
+//    console.log(response); }
+    
+//    button.addEventListener("click", function () {
+//        function insertName(inputCity.value)
+//    });
 
 
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+
+
+
+
